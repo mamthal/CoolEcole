f2c7ed029e82a93c6fa6d9310fb0fdb9a92b9cef
Mamtha Kashyap <rudramatangi@gmail.com>
2016-05-24 16:45:57 -0700

    Ember content

--- app/routes.js
+++ app/routes.js
@@ -1,6 +1,10 @@
+var router = express.Router();
+
 module.exports = function(app){
   app.get('/', function(req,res)
   {
     res.sendFile('./public/index.html');
   });
 };
+
+app.use('api/v1/', router);
